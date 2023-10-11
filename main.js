e3b9d4b738353ce437d444d056639353398201f5
xiachuan945 <j5577919@gmail.com>
2023-10-11 12:12:10 +0800

    import & export

---
+++ main.js
@@ -0,0 +1,12 @@
+// 具名匯出：每個檔案可以有很多個
+export const a = 2;
+
+export const text = "人豪很帥";
+
+const obj = {
+    name: "James",
+    age: 18
+}
+
+// 預設匯出：每個檔案只能用一個
+export default obj;
---
+++ main2.js
@@ -0,0 +1,7 @@
+import { a, text } from "./main.js";
+
+console.log(a)
+console.log(text)
+
+import oxo from "./main.js";
+console.log(oxo)
